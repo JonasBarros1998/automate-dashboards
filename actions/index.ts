b2b38c0191472ef165108b183853d2e031af87ca
Jonas <jonas_barros@outlook.com>
2026-05-11 01:23:56 -0300

    add logs to debug app

--- actions/index.ts
+++ actions/index.ts
@@ -309,7 +309,9 @@ export const loadDatas = (body: string) => {
 /* ================= RUN ================= */
 
 function run() {
+  core.info("invoke run function: ")
   const issue = github.context.payload.issue;
+  core.info("issue: " + issue)
 
   if (issue?.title === "Create Dashboard") {
     const data = JSON.parse(issue.body as string) as Issue;
